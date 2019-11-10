e8c0b3e8e5de46752b71e6402f06acc3adaea85c
Al Madireddy <al.madireddy@gmail.com>
2019-11-10 01:45:15 -0600

    coordinating ready states

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const server = require('http').Server(app);
 const io = require("socket.io")(server);
 
+const LEFT = "leftSide";
+const RIGHT = "rightSide";
+
 server.listen(process.env.PORT || 8080);
 app.use(express.static('public'));
 
@@ -17,24 +20,62 @@ let packetHandler = (packet, fromSide) => {
     return;
   }
 
-  if (fromSide === "leftSide") {
+  // Ready Status messages
+  if (packet.type === "readyMessage") {
+    console.log(`received ready from ${fromSide}`)
+    if (fromSide === LEFT) {
+      leftReady = true;
+    } else if (fromSide === RIGHT) {
+      rightReady = true;
+    }
+
+    if (leftReady && rightReady) {
+      let message = {
+        type: "readyReply",
+        status: "ready"
+      }
+
+      io.emit(LEFT, message);
+      io.emit(RIGHT, message);
+    }
+  }
+
+  if (fromSide === LEFT) {
     packet.messageFromServer = "message from left"
-    io.emit("rightSide", packet);
+    io.emit(RIGHT, packet);
   } 
-  else if (fromSide === "rightSide") {
+  else if (fromSide === RIGHT) {
     packet.messageFromServer = "message from right"
-    io.emit("leftSide", packet)
+    io.emit(LEFT, packet)
   }
 }
 
+let leftReady = false;
+let rightReady = false;
+
+let numberOfClients = 0;
+
 io.on('connection', socket => {
   console.log("a user connected.");
+  numberOfClients++; 
+  console.log(numberOfClients)
 
-  socket.on("leftSide", (packet) => {
-    packetHandler(packet, "leftSide")
-  })
+  socket.on(LEFT, (packet) => {
+    packetHandler(packet, LEFT)
+  });
 
-  socket.on('rightSide', (packet) => {
-    packetHandler(packet, "rightSide")
-  })
+  socket.on(RIGHT, (packet) => {
+    packetHandler(packet, RIGHT)
+  });
+
+  socket.on('disconnect', () => { 
+    console.log("a user disconnected.");
+    numberOfClients--; 
+    console.log(numberOfClients);
+    if (numberOfClients == 0) {
+      leftReady = false;
+      rightReady = false;
+    }
+  });
 })
+
